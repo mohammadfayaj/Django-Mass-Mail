d506ec36fa4714351771b04c72698933ac9c4813
Mohammad Fayaj <61315077+mohammadfayaj@users.noreply.github.com>
2021-04-23 15:08:18 +0600

    Update views.py

--- mass_mail/views.py
+++ mass_mail/views.py
@@ -9,10 +9,10 @@ def send_mail_handler(request):
 	try:
 		mail_objects = SendMail.objects.get()
 
-		email = mail_objects.email.all() 				# unpack email [email is a manytomanyfield]
-		list_of_email = [str(item) for item in email] 	# covert all email in a list
+		email = mail_objects.email.all() 			# unpack email [email is a manytomanyfield]
+		list_of_email = [str(item) for item in email] 		# covert all email in a list
 
-		path = rf'{mail_objects.attach_a_file.path}' 	# convert https:url to os path
+		path = rf'{mail_objects.attach_a_file.path}' 		# convert https:url to os path
 
 		# url = f"{mail_objects.attach_a_file}"			# http url
 
@@ -20,12 +20,12 @@ def send_mail_handler(request):
 
 		    subject = f'{mail_objects.subject}', 		# mail subject
 		    body =	f'{mail_objects.message}',  		# mail message body
-		    from_email= f'{mail_objects.from_email}', 	# mail sender
-		    to = list_of_email,  						# list of recipients
+		    from_email= f'{mail_objects.from_email}', 		# mail sender
+		    to = list_of_email,  				# list of recipients
 		    headers={'Message-ID': '245364'}, 			# extra header
 		)
 
-		email.attach_file(f'{path}') 					# assign attach_file path
+		email.attach_file(f'{path}') 				# assign attach_file path
 		email.send(fail_silently=False)
 
 		messages.info(request, "Mail Send Successfully")
